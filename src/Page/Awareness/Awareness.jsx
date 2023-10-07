585d5eae4c6b18d621eb65ba9642678a98b881d4
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2023-10-07 18:39:50 +0200

    finish Awareness page

--- src/Page/Awareness/Awareness.jsx
+++ src/Page/Awareness/Awareness.jsx
@@ -1,70 +1,38 @@
-import "./Awareness.scss";
-import {  useLayoutEffect, useRef } from "react";
-
-
-
-
+import { AiFillFire, AiFillSave, AiFillFlag } from 'react-icons/ai';
+import {BiMessageDetail} from "react-icons/bi"
+import "./Awareness.css"
 const Awareness = () =>{
-  const mainref = useRef(null);
-  useLayoutEffect(() => {
-    const wrapper = document.body,
-    sections = [...mainref.current.querySelectorAll(".section")],
-    closeButtons = [...mainref.current.querySelectorAll(".close-section")],
-    expandedClass = "is-expanded",
-    hasExpandedClass = "has-expanded-item";
 
-  const initEvents = () => {
-    sections.forEach((element) => {
-      element.addEventListener("click", () => openSection(element));
-    });
-    closeButtons.forEach((element) => {
-      element.addEventListener("click", (event) => {
-        event.stopPropagation();
-        closeSection(element.parentElement);
-      });
-    });
-  };
-
-  const openSection = (element) => {
-    if (!element.classList.contains(expandedClass)) {
-      element.classList.add(expandedClass);
-      wrapper.classList.add(hasExpandedClass);
-    }
-  };
-
-
-  const closeSection = (element) => {
-    if (element.classList.contains(expandedClass)) {
-      element.classList.remove(expandedClass);
-      wrapper.classList.remove(hasExpandedClass);
-    }
-  };
-  initEvents();
-
-  },[])
-  
   return (
-    <main className="main" ref={mainref}>
-      <section className="section">
-        <div className="close-section">&times;</div>
-        <div className="demo-box">
-          <p>How to deal with fire</p>
-          <p className = "isHidden">How are your your</p>
-        </div>
+    <>
+    <main className="container">
+
+      <section className="top">
+          <h1>Awareness</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla iste culpa sint porro dolorem fugiat deserunt quaerat animi, velit ullam, eveniet cum perspiciatis ipsa consectetur fugit corrupti nobis adipisci quisquam quia sunt temporibus non. Asperiores neque ad ut atque, nihil at magnam nostrum eaque laudantium quisquam aliquid animi a aliquam?</p>
       </section>
-      <section className="section">
-        <div className="close-section">&times;</div>
-        <div className="demo-box">How to evalute from fire</div>
+      <section className="main-content">
+            <div className="card-1">
+                <AiFillFire />
+                <p>how to deal with fire</p>
+            </div>
+            <div className="card-2">
+                <AiFillSave/>
+                <p>How to Evactuate from fire</p>
+            </div>
+            <div className="card-3">
+              <AiFillFlag/>
+              <p>How to use the app to confire a fire</p>
+            </div>
+            <div className="card-4">
+              <BiMessageDetail/>
+              <p>Different Fire situtation</p>
+            </div>
       </section>
-      <section className="section">
-        <div className="close-section">&times;</div>
-        <div className="demo-box">How to use the app to confirm fire</div>
-      </section>
-      <section className="section">
-        <div className="close-section">&times;</div>
-        <div className="demo-box">Different fire situations</div>
-      </section>
-</main>
+    </main>
+
+  </>
+
   )
 }
 
